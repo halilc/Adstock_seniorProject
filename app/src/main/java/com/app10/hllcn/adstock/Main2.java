ce56cbf2c45f718f2b47d51f0762d1582eb04347
hllcn <halilc.gunduz@gmail.com>
2018-05-27 00:25:26 +0300

    User-id and Campaings

--- app/src/main/java/com/app10/hllcn/adstock/Main2.java
+++ app/src/main/java/com/app10/hllcn/adstock/Main2.java
@@ -3,6 +3,7 @@ package com.app10.hllcn.adstock;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -16,9 +17,11 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Base64;
 import android.util.Log;
-
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -26,7 +29,6 @@ import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.facebook.AccessToken;
 import com.github.johnpersano.supertoasts.library.SuperToast;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
@@ -40,8 +42,19 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.onesignal.OneSignal;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -53,40 +66,27 @@ public class Main2 extends Activity {
     private long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
     private long FASTEST_INTERVAL = 10000; /* 2 sec */
     private RequestQueue queue;
-    private String url;
-
-    public static AccessToken token;
+    private String url,url2;
+    private String[] interest_list;
     private String msg;
+
     private Button facebook, test,personal,showcamp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main2);
         startLocationUpdates();
-
         test = findViewById(R.id.test);
         personal = findViewById(R.id.personal);
         showcamp = findViewById(R.id.showcamp);
-        facebook = (Button) findViewById(R.id.facebook);
         queue = Volley.newRequestQueue(this);  // this = context
-        url = "http://178.62.210.159:8000/api/send_location";
+        url = "http://178.62.210.159:8000/api/user_register";
+        url2 = "http://178.62.210.159:8000/api/send_location";
         OneSignal.startInit(getApplicationContext())
                 .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
                 .unsubscribeWhenNotificationsAreDisabled(true)
                 .init();
-
-        facebook.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(Main2.this, facebook.class);
-                startActivity(intent);
-            }
-        });
-        token  = com.app10.hllcn.adstock.facebook.token;
-
-
         test.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -94,11 +94,26 @@ public class Main2 extends Activity {
                 startActivity(intent);
             }
         });
+        interest_list = new String[]{"Arts & Entertainment","College & University","Event","Food","Nightlife","Spot","Outdoors & Recreation","Professional & Other","Places","Residence","Shop & Service","Travel & Transport"};
+        String choose;
         personal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Main2.this, personal.class);
-                startActivity(intent);
+                new MaterialDialog.Builder(Main2.this)
+                        .title("Personal")
+                        .items(interest_list)
+                        .itemsCallbackMultiChoice(null, new MaterialDialog.ListCallbackMultiChoice() {
+                            @Override
+                            public boolean onSelection(MaterialDialog dialog, Integer[] which, CharSequence[] text) {
+                                Integer in[];
+                                in = dialog.getSelectedIndices();
+                                Log.e("select", Arrays.toString(in));
+                                httppost(Arrays.toString(in));
+                                return true;
+                            }
+                        })
+                        .positiveText(R.string.choose)
+                        .show();
             }
         });
         showcamp.setOnClickListener(new View.OnClickListener() {
@@ -109,6 +124,109 @@ public class Main2 extends Activity {
             }
         });
 
+    }
+    public void httppost( final String interests){
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.e("************",response);
+                        writeToFile(response);
+
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        //Log.d("Error.Response", response);
+                        VolleyLog.d("ErrorVolley", "Error: " + error.getStackTrace());
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("interests", interests.toString());
+                return params;
+            }
+        };
+        queue.add(postRequest);
+    }
+    public void httppost2( final Double latitude ,final Double longitude){
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url2,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        //Log.d("Error.Response", response);
+                        VolleyLog.d("ErrorVolley", "Error: " + error.getStackTrace());
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("user_id",readFromFile() );
+                params.put("lat", latitude.toString());
+                params.put("long", longitude.toString());
+
+                return params;
+            }
+        };
+        queue.add(postRequest);
+    }
+    private void writeToFile(String data) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(getApplicationContext().openFileOutput("adstock.json", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+    private String readFromFile() {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = getApplicationContext().openFileInput("adstock.json");
+            Log.e("file",inputStream.toString());
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
     }
     protected void startLocationUpdates() {
 
@@ -137,7 +255,6 @@ public class Main2 extends Activity {
                 },
                 Looper.myLooper());
     }
-
     public void onLocationChanged(Location location) {
         msg = "Location: " +
                 Double.toString(location.getLatitude()) + "," +
@@ -149,42 +266,8 @@ public class Main2 extends Activity {
         superToast.show();
         //Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-        //Log.e("--->>",msg);
-        httppost(location.getLatitude(),location.getLongitude());
-    }
-
-    public void httppost( final Double latitude ,final Double longitude){
-        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>()
-                {
-                    @Override
-                    public void onResponse(String response) {
-                        // response
-                        Log.d("Response", response);
-                    }
-                },
-                new Response.ErrorListener()
-                {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // error
-                        //Log.d("Error.Response", response);
-                        VolleyLog.d("ErrorVolley", "Error: " + error.getStackTrace());
-                    }
-                }
-        ) {
-            @Override
-            protected Map<String, String> getParams()
-            {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("user_id","2" );
-                params.put("lat", latitude.toString());
-                params.put("long", longitude.toString());
-
-                return params;
-            }
-        };
-        queue.add(postRequest);
+        Log.e("--->>",msg);
+        httppost2(location.getLatitude(),location.getLongitude());
     }
     public void getLastLocation() {
 
@@ -192,7 +275,6 @@ public class Main2 extends Activity {
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //   Permission eklenip düzenlencek
-
             return;
         }
         locationClient.getLastLocation()
